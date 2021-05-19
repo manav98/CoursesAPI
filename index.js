69a00e6c9b33c69cb3a780a013fca5ad1610d5c7
Manav <manav.guptamg14@gmail.com>
2021-05-19 23:49:10 +0530

    added validation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express')
 const app = express()
 
@@ -21,10 +22,16 @@ app.get('/api/courses', (req, res) => {
 app.get('/api/courses/:id', (req, res) => {
     let course = courses.find(c => c.id === parseInt(req.params.id))
     if (!course) { res.status(404).send('The course with given id was not found'); }
-    else { res.send(course); }
+    res.send(course);
 })
 
 app.post('/api/courses', (req, res) => {
+    const { error } = validateCourse(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
     const course = {
         id: courses.length + 1,
         name: req.body.name,
@@ -33,5 +40,27 @@ app.post('/api/courses', (req, res) => {
     res.send(course);
 })
 
+app.put('/api/courses/:id', (req, res) => {
+    let course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) { res.status(404).send('The course with given id was not found'); }
+
+    const { error } = validateCourse(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    course.name = req.body.name;
+    res.send(course);
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`))
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+
+    return Joi.validate(course, schema);
+}
